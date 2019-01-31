04d294f93c949003d50f7ed0d6260a7dc8bc2d67
stuart gill <stuart.a.gill@gmail.com>
2019-01-30 17:29:55 -0800

    refactor script.js

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ let num = prompt("Enter a number");
 num = parseInt(num);
 console.log(twoSpeed(num));
 
-function twoSpeed(n) {
-    let answerSet = [1,2];
+function twoSpeed(stairs) {
+    let answerSet = [0,1,2];
 
-    if (n>2){
-        for(let i=1;i<(n-1);i++){
+    if (stairs>2){
+        for(let i=2;i<(stairs);i++){
             let newElement = answerSet[i] + answerSet[i-1];
             answerSet.push(newElement);
         }
     }
-    return(`num of combos is ${answerSet[n-1]}`);
+    return(`num of combos is ${answerSet[stairs]}`);
  }
 
 
